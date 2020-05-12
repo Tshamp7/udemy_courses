a64663d74c3bb8fe98ced535b0abf17f0e4c8be3
Tom <T.shamp7@gmail.com>
2020-05-12 10:24:29 -0700

    inner join v3

--- postgresQL/inner_join.sql
+++ postgresQL/inner_join.sql
@@ -21,3 +21,35 @@ ORDER BY movies.movie_length;
 	and which table is specified as table 2.
 	
 	*/
+	
+/*
+There is also a shorter way to write your join query using aliases for the table names.
+
+SELECT t1.column1, t1.column2, t2.column1 FROM table1 t1
+JOIN table2 t2 ON t1.column3 =t2.column3;
+*/
+
+SELECT d.director_id, d.first_name, d.last_name, mo.movie_name 
+FROM directors d
+INNER JOIN movies mo
+	ON d.director_id = mo.director_id
+WHERE mo.movie_lang = 'Japanese'
+ORDER BY mo.movie_length;
+
+-- The following will return the movie name and domestic and international takings for each film.
+-- NOTE JOIN and INNER JOIN are synonymous as INNER JOIN is typically the most common join
+-- used in sql.
+
+SELECT mo.movie_name, mr.domestic_takings, mr.international_takings FROM movies mo
+JOIN movie_revenues mr ON mo.movie_id = mr.movie_id
+ORDER BY mr.domestic_takings;
+
+-- The following syntax 'USING' will ONLY work when the joining columns have the same name.
+-- This will have the same output as the previous example. 
+/*
+SELECT t1.column1, t1.column2, t2.column1 FROM table1 t1
+JOIN table2 t2 USING (column3);
+*/
+
+SELECT mo.movie_name, mr.domestic_takings FROM movies mo
+JOIN movie_revenues mr USING (movie_id);
