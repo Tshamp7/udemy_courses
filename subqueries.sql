a27dd6e81c2fdef8348015b594441e9019a63573
Tom <T.shamp7@gmail.com>
2020-05-12 14:58:14 -0700

    subqueries pt 1

---
+++ subqueries.sql
@@ -0,0 +1,35 @@
+-- subqueries part 1
+
+/* 
+subqueries are nested queries, or, queries within queries. 
+The inner select query will ALWAYS be exectuted first and these 
+results are passed to the outer select query. 
+
+Uncorrelated subqueries are subqueries that could be run on their own.
+
+Correlated subqueries CANNOT run without the outer query. This is because
+the inner query references a table from the outer query. For example an inner
+query could reference table aliases defined in the first, or outer query. 
+
+*/
+
+-- The following query is an example of an uncorrelated subquery.
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length > 
+(SELECT AVG(movie_length) FROM movies);
+
+SELECT first_name, last_name, date_of_birth
+FROM directors
+WHERE date_of_birth > (
+	SELECT date_of_birth 
+	FROM directors
+	WHERE CONCAT(first_name, ' ', last_name) = 'James Cameron'
+);
+
+-- Sometimes, it can be helpful to write the inner subquery first, and then
+-- write the outer subqeury around it. 
+
+-- It can also be helpful to run your inner query first seperately,
+-- this will help to make sure you get the correct return from the inner query.
+
